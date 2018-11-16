57f748cda687f70de22ead58b0437fd9061826fb
Jessie Huff <huffjd1@gmail.com>
2018-11-16 16:04:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,46 @@
-// Code your solution here!
+function printString(myString) {
+    console.log(myString[0]); 
 
+    if (myString.length > 1) {
+        let mySubString = myString.substring(1, myString.length); 
+        printString(mySubString);
+    } else {
+        return true; 
+    }
+}
+
+function reverseString(myString) {
+    if (myString.length < 2){
+        return myString; 
+    } else {
+        return reverseString(myString.substring(1)) + myString[0] 
+    }
+}
+
+function isPalindrome(myString){
+    if (myString.length < 2) {
+        return true 
+    }
+    else if (myString[0] === myString[myString.length-1]) {
+        return isPalindrome(myString.substring(1, myString.length-1))
+    }
+    else { return false }
+}
+
+function addUpTo(array, index){
+    let number = array[index] 
+    if (index > 0) {
+        return number + addUpTo(array, index-1)
+    }
+    else {
+        return number; 
+    }
+}
+
+function maxOf(array){
+    
+}
+
+function includesNumber(){
+
+}
